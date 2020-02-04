10f295514e8f02a42b741ffc86eb6b1405ad1edd
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-04 15:26:40 -0700

    Case two step six

---
+++ case2/hg_report.js
@@ -0,0 +1,16 @@
+"use strict";
+
+/*
+   New Perspectives on HTML5 and CSS3, 7th Edition
+   Tutorial 10
+   Case Problem 2
+
+   Author: Jorge Collazo
+   Date:   2-4-20
+   
+   Filename: hg_report.js
+	
+*/
+
+var gameReport = "<h1>" + itemTitle + "</h1><h2>By:" + itemManufacturer + "</h2><img src='hg_" + itemID + ".png' alt='" + itemID +"' id='gameImg' /><table><tr><th>Product ID</th<th>" + itemID + "</th></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
+
