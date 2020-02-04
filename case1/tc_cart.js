7cf9c00ec974e48c8327932e1879f72228fd9d71
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-04 15:01:19 -0700

    Finished case one

--- case1/tc_cart.js
+++ case1/tc_cart.js
@@ -15,10 +15,17 @@
 //Total cost of the order
 var orderTotal = 0;
 //Declares the contents of the shoping cart
-var cartHTML = "<table><tr><th>Item</th><th>Description</th>Price</th><th>Qty</th><th>Total</th></tr>"
+var cartHTML = "<table><tr><th>Item</th><th>Description</th><th>Price</th><th>Qty</th><th>Total</th></tr>"
 //
 for(var i=0; i<item.length; i++){
    //come back to check step 12 a.
-   cartHTML = "<tr><td><img src='" + tc_item.png + "' alt='" + item[i] + "'/></td>"
-   cartHTML +="<td>" + itemDescription[i] + "</td><td>" + $itemPrice[i] + "</td><td>" + itemQty[i] + "</td>"
+   cartHTML += "<tr><td><img src='tc_" + item[i] + ".png' alt='" + item[i] + "'/></td>"
+   cartHTML += "<td>" + itemDescription[i] + "</td><td>$" + itemPrice[i] + "</td><td>" + itemQty[i] + "</td>"
+   var itemCost = itemPrice[i] * itemQty[i]
+   cartHTML += "<td>$" + itemCost + "</td></tr>"
+   itemCost += orderTotal
 }
+
+cartHTML += "<tr><td colspan='4'>Subtotal</td><td>$" + orderTotal + "</td></tr></table"
+
+document.getElementById("cart").innerHTML = cartHTML
