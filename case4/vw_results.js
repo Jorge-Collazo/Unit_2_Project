35663e040154efe02e03e6066d8be7ce420e4ce2
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-05 15:24:11 -0700

    Step six A on case Four

--- case4/vw_results.js
+++ case4/vw_results.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Case Problem 4
 
-   Author: 
-   Date:   
+   Author: Jorge Collazo
+   Date:   2-5-20
    
    Filename: vw_results.js
    
@@ -23,7 +23,9 @@
    
       
 */
+var reportHTML = "<h1>" + raceTitle + "</h1>"
 
+for(var i = 0; )
 
 
 
