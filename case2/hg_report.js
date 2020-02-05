7f6588b0592e084adcf5099b80bc206f3e1053cd
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-05 13:56:26 -0700

    Finished Case Two

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -14,3 +14,24 @@
 
 var gameReport = "<h1>" + itemTitle + "</h1><h2>By:" + itemManufacturer + "</h2><img src='hg_" + itemID + ".png' alt='" + itemID +"' id='gameImg' /><table><tr><th>Product ID</th<th>" + itemID + "</th></tr><tr><th>List Price</th><td>" + itemPrice + "</td></tr><tr><th>Platform</th><td>" + itemPlatform + "</td></tr><tr><th>ESRB Rating</th><td>" + itemESRB + "</td></tr><tr><th>Condition</th><td>" + itemCondition + "</td></tr><tr><th>Release</th><td>" + itemRelease + "</td></tr></table>" + itemSummary;
 
+document.getElementsByTagName("article")[0].innerHTML = gameReport
+
+var ratingSum = 0;
+var ratingCount = [];
+
+for(var i = 0; i<ratings.length; i++){
+   ratingSum += ratings[i]
+}
+
+var ratingAvg = ratingSum / ratingCount[i];
+var ratingReport = "<h1>Customer Reviews</h1><h2> " + ratingAvg + " out of 5 stars (" + ratingCount + " reviews)</h2>";
+
+for(var i = 0; i < 3; i++){
+   ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr><tr><th>Review Date </th><td>" + ratingDates[i] + "</td></tr><tr><th>Rating</th><td>"
+   for(var j = 1; j <= ratings[i]; j++){
+      ratingReport += "<img src='hg_star.png'/>"
+   }
+   ratingReport += "</td></tr></table>" + ratingSummaries[i] + "</div>"
+}
+
+document.getElementsByTagName("aside")[0].innerHTML = ratingReport;
