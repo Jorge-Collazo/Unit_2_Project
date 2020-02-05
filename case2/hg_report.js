541979d6366511728b53aeae9220b8406261fab4
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-05 14:26:37 -0700

    Fixed case two

--- case2/hg_report.js
+++ case2/hg_report.js
@@ -17,17 +17,17 @@ var gameReport = "<h1>" + itemTitle + "</h1><h2>By:" + itemManufacturer + "</h2>
 document.getElementsByTagName("article")[0].innerHTML = gameReport
 
 var ratingSum = 0;
-var ratingCount = [];
+var ratingCount = ratings.length;
 
 for(var i = 0; i<ratings.length; i++){
    ratingSum += ratings[i]
 }
 
-var ratingAvg = ratingSum / ratingCount[i];
+var ratingAvg = ratingSum / ratingCount;
 var ratingReport = "<h1>Customer Reviews</h1><h2> " + ratingAvg + " out of 5 stars (" + ratingCount + " reviews)</h2>";
 
 for(var i = 0; i < 3; i++){
-   ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr><tr><th>Review Date </th><td>" + ratingDates[i] + "</td></tr><tr><th>Rating</th><td>"
+   ratingReport += "<div class='review'><h1>" + ratingTitles[i] + "</h1><table><tr><th>By</th><td>" + ratingAuthors[i] + "</td></tr><tr><th>Review Date </th><td>" + ratingDates[i] + "</td></tr><tr><th>" + ratings[i] + "</th><td>"
    for(var j = 1; j <= ratings[i]; j++){
       ratingReport += "<img src='hg_star.png'/>"
    }
--- case3/ah_report.js
+++ case3/ah_report.js
@@ -5,8 +5,8 @@
    Tutorial 10
    Case Problem 3
 
-   Author:  
-   Date:   
+   Author:  Jorge Collazo
+   Date:   2-5-20
    
    Filename: ah_report.js
    
