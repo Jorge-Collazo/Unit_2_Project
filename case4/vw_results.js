c58e58c7de80a6eba5d5ce6cfa7e3eb44284a1cf
Jorge Collazo <jcollazo151@west-mec.org>
2020-02-06 13:56:29 -0700

    Finished Case Four
    and I have never felt so relieved

--- case4/vw_results.js
+++ case4/vw_results.js
@@ -29,13 +29,27 @@ for(var i = 0; i < race.length; i++){
    var totalVotes = 0;
    votes[i].forEach(calcSum)
    reportHTML += "<table><caption>" + race[i] + "</caption><tr><th>Candidates</th><th>Votes</th></tr>"
-   //step 6 D
-   reportHTML += "</table>"
+   reportHTML += candidateRows(i, totalVotes);
+   reportHTML += "</table>";
 }
 
-document.getElementsByName("section").innerHTML = reportHTML
+document.getElementsByTagName("section")[0].innerHTML = reportHTML
 
-
+function candidateRows(raceNum, totalVotes) {
+   var rowHTML = ""
+   for(var j = 0; j < 3; j++){
+      var candidateName = candidate[raceNum][j];
+      var candidateParty = party[raceNum][j];
+      var candidateVotes = votes[raceNum][j];
+      var candidatePercent = calcPercent(candidateVotes, totalVotes);
+      rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + ")</td>"
+      for(var k = 0; k<candidatePercent; k++){
+         rowHTML += createBar(candidateParty);
+      }
+      rowHTML += "</tr>";
+   }
+   return rowHTML;
+}
 
 
 /* Callback Function to calculate an array sum */
@@ -48,3 +62,14 @@ function calcPercent(value, sum) {
    return (100*value/sum);
 }
 
+function createBar(partyType){
+   var barHTML = "";
+   if(partyType === "D"){
+      barHTML += "<td class='dem'></td>"
+   }else if(partyType === "R"){
+      barHTML += "<td class='rep'></td>"
+   }else if(partyType === "I"){
+      barHTML += "<td class='ind'></td>"
+   }
+   return barHTML;
+}
